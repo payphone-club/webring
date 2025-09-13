6d7da13e9c8109712292033a9e7ebc95fa98a805
Mike Dank <Famicoman@gmail.com>
2025-09-13 13:51:26 -0400

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,37 +1,12 @@
 let sites =
 	[{
-		name: "æstrid",
-		url: 'https://astrid.smith.seattle.wa.us',
-		tagline: "billing excellence is a full time job"
+		name: "RandTel",
+		url: 'https://randtel.co/',
+		tagline: "The Randolph Public Telephone Operating Company"
 	}
 	,{
-		name: "spacegirl",
-		url: "https://witch.spmh.us",
-		tagline: "i have no idea"
-	}
-	,{
-		name: "DashTel",
-		url: "https://dashtel.ca",
-		tagline: "You've reached DashTel, HatbowlDash speaking, how may I direct your call?"
-	}
-	,{
-		name: "trmg",
-		url: "https://vofr.net",
-		tagline: "Only you can prevent toll fraud."
-	}
-	,{
-		name: "DMS-100.net",
-		url: "http://www.dms-100.net",
-		tagline: "Adventures in setting up a lab DMS-100."
-	}
-        ,{
-		name: "PlayaTel BBS",
-		url: "https://bbs.playa.tel",
-		tagline: "Another great service from your CO here at 7:30 & C"
-	}
-	,{
-		name: "k4nfp.net",
-		url: "https://k4nfp.net",
-		tagline: "a conglomeration of weird old telephone stuff, with some RF sprinkled in.."
+		name: "PhilTel",
+		url: 'https://philtel.org/',
+		tagline: "A Phreaky Phone Collective"
 	}
 ]
